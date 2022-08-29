ca02a4ecd7f512121f46d58f54ef0c48c638d5ff
Nadim-2022 <112079541+Nadim-2022@users.noreply.github.com>
2022-08-29 14:49:18 +0300

    TESt commit

---
+++ Exercises 3.py
@@ -0,0 +1,24 @@
+# phase 1
+zander = float(input("Please give the length of zander in centimeters"))
+min_l = 42
+if zander >= 42:
+    print("OK")
+if zander < 42:
+    print(f"Release the zander back into the lack, because zander shor by  {42-zander}")
+
+# phase 2
+cabin_class = ("LUX", "A", "B", "C", "D")
+LUX = input("Enter the cabin class of a cruise ship:")
+A = input("Enter the cabin class of a cruise ship:")
+B = input("Enter the cabin class of a cruise ship:")
+C = input("Enter the cabin class of a cruise ship:")
+if LUX == LUX:
+    print("Upper-deck cabin with a balcony.")
+if A == A:
+    print("Above the car deck, equipped with a window.")
+if B == B:
+    print("Windowless cabin above the car deck.")
+if C == C:
+    print("Windowless cabin below the car deck.")
+else:
+    print("Invalid cabin class")
