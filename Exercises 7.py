921713d29a4b6d8da99a9cdbe97052cf611deb4c
Nadim-2022 <i.am.nadim95@gmail.com>
2022-09-26 01:20:25 +0300

    Exercises 7

--- Exercises 7.py
+++ Exercises 7.py
@@ -28,7 +28,7 @@ print(names)
 
 
 """""
-Write a program for fetching and storing airport data. The program asks the user if they want to enter a new airport, fetch the information of an existing airport or quit.
+3. Write a program for fetching and storing airport data. The program asks the user if they want to enter a new airport, fetch the information of an existing airport or quit.
 If the user chooses to enter a new airport, the program asks the user to enter the ICAO code and name of the airport.
 If the user chooses to fetch airport information instead, the program asks for the ICAO code of the airport and prints out the corresponding name. 
 If the user chooses to quit, the program execution ends. The user can choose a new option as many times they want until they choose to quit. 
